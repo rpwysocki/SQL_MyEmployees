d32ab5ec97c0271cecf29a559880be6f1380ceff
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-07-19 10:24:57 -0400

    wrote out queries, adjusting index

--- index.js
+++ index.js
@@ -14,15 +14,22 @@ class DB{
 
 
 
-
-
-
-
-
-
-
-
-
 }
 
 module.exports = new DB(connection);
+
+
+
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const connection = require("./connection");
+const employeeController = require("./controllers/employeeController");
+const fs = require("fs");
+
+
+fs.readFile("./db/schema.sql", "utf8", (err, data) => {
+    if (err) throw err;
+});
+
+employeeController.mainMenu();
