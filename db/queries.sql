2b1423f5964cf23ac93068867f284f5baa52460e
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-07-18 23:55:07 -0400

    reset file tree, added dependancies, worked on queries

--- db/queries.sql
+++ db/queries.sql
@@ -2,3 +2,4 @@ SELECT manager_id
     CASE manager_id WHEN INT THEN employee(id)
                     
     * `manager_id`: `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
+
